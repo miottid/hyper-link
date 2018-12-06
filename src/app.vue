e3de7e3d25dfbdb195c85bad2d5932b3a573f0ca
Quentin Bellanger <hello@quentin-bellanger.com>
2018-12-06 22:34:31 +0100

    [+] Improve page examples

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <nav>
+    <div class="content">
+      <h1><code>&lt;hyper-link&gt;</code></h1>
       <ul>
-        <li><hyper-link href="/foo" title="Foo" /></li>
-        <li><hyper-link href="/bar" title="Bar" /></li>
-        <li><hyper-link href="https://muxumuxu.com" title="Muxu.Muxu" target="_blank" rel="noopener" /></li>
+        <li><hyper-link title="Default link" /></li>
+        <li><hyper-link href="/foo" title="Relative link" /></li>
+        <li><hyper-link href="https://github.com/muxumuxu/hyper-link" title="External link" /></li>
+        <li><hyper-link href="mailto:?subject=Hello%20World" title="Mailto link" /></li>
+        <li><hyper-link href="tel:+33000000000" title="Tel link" /></li>
       </ul>
-    </nav>
-    <router-view />
+    </div>
   </div>
 </template>
 
@@ -23,12 +25,33 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background: rgba(0, 0, 0, 0.1);
+  font-family: sans-serif;
+  min-height: 100vh;
+  padding: 3rem 1rem;
+}
+
+.content {
+  background: white;
+  border-radius: 0.25rem;
+  margin: 0 auto;
+  max-width: 40rem;
+  padding: 1rem;
+}
+
+h1 {
+  margin: 0 0 1rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+code {
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 0.25rem;
+  padding: 0.25rem;
+}
+
+ul {
+  margin: 0;
 }
 </style>
